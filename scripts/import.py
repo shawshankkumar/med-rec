9bc19d07ee73fe92a6255366fb95ce5391ef666c
Shashank Kumar <shashankkumarthakur@gmail.com>
2023-10-13 01:18:23 +0530

    Create import.py

---
+++ scripts/import.py
@@ -0,0 +1,37 @@
+import csv
+from neo4j import GraphDatabase
+
+drugs = {}
+
+
+def removeWhiteSpace(x): return x.strip()
+
+
+# open the CSV file
+with open('data.csv', 'r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        drugs[row["drug_name"]] = {
+            "medical_condition": row['medical_condition'],
+            "drug_classes": list(map(removeWhiteSpace, row['drug_classes'].split(','))),
+            "description": row['medical_condition_description'],
+        }
+
+driver = GraphDatabase.driver(
+    "neo4j+s://86a7898d.databases.neo4j.io", auth=("neo4j", "ISNS7JjtG22Atg_6I6PVvg6P9b1wIpWN1Z7HsOVdkEA"))
+
+# define a function to create a node
+
+
+def create_nodes(tx, drugName, medicalCondition, drugClasses, description):
+    result = tx.run("CREATE (d:Drug {name: $name, drugClasses: $drugClasses, description: $description}) RETURN ID(d) as id",
+                    name=drugName, drugClasses=drugClasses, description=description)
+    id = result.single()["id"]
+    tx.run(
+        "MATCH (d:Drug) WHERE ID(d)=$id MERGE (m:MedicalCondition {name: $name}) CREATE (d)-[r:TREATS]->(m)", id=id, name=medicalCondition)
+
+
+for key, value in drugs.items():
+    with driver.session() as session:
+        session.execute_write(
+            create_nodes, key, drugs[key]["medical_condition"], drugs[key]["drug_classes"], drugs[key]["description"])
