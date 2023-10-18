d41ce338e5184eef5f79ad8bf336433122a9684a
unknown <aj9361@srmist.edu.in>
2023-10-18 23:04:22 +0530

    chore: handle errors

--- src/components/InputArea.tsx
+++ src/components/InputArea.tsx
@@ -29,6 +29,7 @@ export const InputArea = () => {
 
   return (
     <>
+     {error ? <p>{error}</p>   : (
       <div className="flex flex-col space-y-6 mt-64">
         <input
           className="w-96 border-black border-2 p-2.5 focus:outline-none focus:shadow-[2px_2px_0px_rgba(0,0,0,1)] focus:bg-[#FFA6F6] active:shadow-[2px_2px_0px_rgba(0,0,0,1)] rounded-full text-black"
@@ -46,10 +47,10 @@ export const InputArea = () => {
                 </svg>
           ) : "Search Medicine"}
         </button>
-      </div>
-      {error && <p>Error: {error}</p>}
-      <TableData search={search} />
+      </div> )
+          }
+      <TableData search={search} /> 
     </>
+      
   );
 };
-
